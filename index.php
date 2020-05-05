bbb1bbf6161bc3233f157ec3a1900d88a886863a
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-05-05 17:37:54 +0200

    ex5 partie1 php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$answer = 'yes';
+$answer1 = 'no';//déclaration variable
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex1 partie1 php</title>
+</head>
+<body>
+  <p>vous avez répondu
+  <?php
+  echo $answer;
+  echo $answer1;
+  ?>
+</p>
+</body>
+</html>
